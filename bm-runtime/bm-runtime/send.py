3f2faa432bd23be2570e70d2f5f497211d0e7e8d
ricardoparizotto <ricardo.dparizotto@gmail.com>
2019-09-16 19:08:43 +0000

    lateny calculation

--- bm-runtime/bm-runtime/send.py
+++ bm-runtime/bm-runtime/send.py
@@ -32,7 +32,7 @@ def main():
  
     i = 0
  
-    for i in range(4):
+    for i in range(1000):
 	    print "sending on interface %s to %s" % (iface, str(addr))
 	    pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
 	    pkt = pkt /IP(dst=addr) / TCP(dport=1234, sport=random.randint(49152,65535)) / sys.argv[2]
